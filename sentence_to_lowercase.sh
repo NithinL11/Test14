96478d83bb6caadec7e94b1311caf5111690151a
root <root@LAPTOP-33N12T6F.>
2025-02-01 11:52:06 -0500

    Now the script is updated with all script

--- sentence_to_lowercase.sh
+++ sentence_to_lowercase.sh
@@ -5,3 +5,9 @@ RESULT=$(./to_lowercase.sh "$1")
 
 # Print the result
 echo "Lowercase: $RESULT"
+
+# Check if the user provided an input sentence
+if [ -z "$1" ]; then
+    echo "Usage: $0 \"HELLO WORLD\""
+    exit 1
+fi
